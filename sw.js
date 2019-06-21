2b99a0ab7e27935a561e7f9d9ed458aae994b061
DragonOS-Manager <52032586+DragonOS-Manager@users.noreply.github.com>
2019-06-21 17:56:26 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-
+var cacheList=[
+  
+  'index.html'
+  
+  
+];
+self.addEventListener('install',e =>{
+  e.waitUntil(
+    caches.open(cacheStorageKey)
+    .then(cache => cache.addAll(cacheList))
+    .then(() => self.skipWaiting())
+  )
+})
